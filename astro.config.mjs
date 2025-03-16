958fb858b17cab22bd94e587c69dadd6928dee9d
Alexander Eckert <eckertalex@proton.me>
2025-03-16 16:39:51 +0100

    remove unneeded styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 import { siteConfig } from "./src/siteConfig";
-
 import sitemap from "@astrojs/sitemap";
-
 import metaTags from "astro-meta-tags";
+import devtoolBreakpoints from "astro-devtool-breakpoints";
 
 export default defineConfig({
   site: siteConfig.site,
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap(), metaTags()],
+  integrations: [sitemap(), metaTags(), devtoolBreakpoints()],
 });
+
